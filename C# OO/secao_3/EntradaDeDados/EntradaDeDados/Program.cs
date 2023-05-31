54b7b995df7f0afff4f9aec7e93620253102fdab
Felipe Fazan <felipeffazandebrito@gmail.com>
2023-05-31 14:57:28 -0300

    teste

---
+++ C# OO/secao_3/EntradaDeDados/EntradaDeDados/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntradaDeDados {
+    class Program {
+        static void Main(string[] args) {
+
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+            string s = Console.ReadLine();
+
+            string[] vetor = s.Split(' ');
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
