fdaea4f9990feb2d015c744b838ae328b6a35c17
Felipe Fazan <felipeffazandebrito@gmail.com>
2023-07-27 10:56:58 -0300

    continuacao

--- C# OO/secao_3/EntradaDeDados/EntradaDeDados/Program.cs
+++ C# OO/secao_3/EntradaDeDados/EntradaDeDados/Program.cs
@@ -13,15 +13,21 @@ namespace EntradaDeDados {
             string y = Console.ReadLine();
             string z = Console.ReadLine();
 
-            string s = Console.ReadLine();
 
-            string[] vetor = s.Split(' ');
+            string[] v = Console.ReadLine().Split(' ');//esse 
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
 
             Console.WriteLine("Você digitou: ");
             Console.WriteLine(frase);
             Console.WriteLine(x);
             Console.WriteLine(y);
             Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
         }
     }
 }
